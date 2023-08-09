51b6e8bd211c6df2bbaa48ace9d2ae52ac5a2ac2
Gabriel Solari <90011796+gsolarim@users.noreply.github.com>
2023-08-08 20:10:03 -0500

    Function Scope

---
+++ src/scope/function.js
@@ -0,0 +1,12 @@
+function greeting(){
+    let userName = 'Gabriel';
+    console.log(userName);
+
+    if (userName === 'Gabriel'){
+        console.log(`Hello ${userName}!`)
+    }
+
+}
+
+greeting();
+console.log(userName);
